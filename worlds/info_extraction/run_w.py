d1293ec380ee1ffb2b0fbe51ea0f249d37779854
Stefano Melacci <stefano.melacci@unisi.it>
2025-12-16 17:15:13 +0100

    minor fix

--- worlds/info_extraction/run_w.py
+++ worlds/info_extraction/run_w.py
@@ -3,10 +3,13 @@ from unaiverse.networking.node.node import Node
 
 # World
 world = WWorld()
-# world.debug_stats_dashboard()
 
 # Node hosting world
 node = Node(node_name="InfoExtraction", hidden=True, hosted=world, clock_delta=1. / 10.)
 
 # Running node
 node.run()
+
+
+
+
