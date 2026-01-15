9a1bebd02a12af6fadee27b40eca558aab0b8750
Stefano Melacci <stefano.melacci@unisi.it>
2026-01-15 15:14:43 +0100

    fixed turing world - final messages

--- worlds/turing/src/agent.py
+++ worlds/turing/src/agent.py
@@ -29,9 +29,9 @@ class WAgent(Agent):
     # Generic options to configure the Turing Test Hotel
     profile_link = ("https://docs.google.com/forms/d/e/1FAIpQLScF6FuSMDFpowk3bfLzrr35tGErxd864Rf7FuZI9ic8p-nQAg/"
                     "viewform?usp=pp_url&entry.1591917462=<email>")
-    test_duration = 40  # Seconds
+    test_duration = 60  # Seconds
     survey_reply_time = 1 * 30  # Seconds
-    guests_per_room = 2
+    guests_per_room = 4
     tot_rooms = 3
     manager_fake_name = "MANAGER"
     sender_prefix = "**"
