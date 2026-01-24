17c32337719ac323b0919d0f35bda586d14e0502
Stefano Melacci <stefano.melacci@unisi.it>
2026-01-24 19:14:12 +0100

    working on new messages

--- worlds/turing/run_w.py
+++ worlds/turing/run_w.py
@@ -5,8 +5,8 @@ from unaiverse.networking.node.node import Node
 world = WWorld()
 
 # Node hosting world
-node = Node(node_name="TuringHotel_", hosted=world, hidden=True, clock_delta=1. / 50.,
-            world_masters_node_names=["TuringManager_"])
+node = Node(node_name="TuringHotel", hosted=world, hidden=True, clock_delta=1. / 50.,
+            world_masters_node_names=["TuringManager"])
 
 # Running node
 node.run()
