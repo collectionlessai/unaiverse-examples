7abd002001d35aa663a9d498fc877a520af1b3ee
Stefano Melacci <stefano.melacci@unisi.it>
2026-05-07 16:36:19 +0200

    fixed run_w of signal school

--- worlds/signal_school/run_w.py
+++ worlds/signal_school/run_w.py
@@ -5,7 +5,7 @@ from unaiverse.networking.node.node import Node
 world = WWorld()
 
 # Node hosting world
-node = Node(node_id="Test0", hosted=world, hidden=True, clock_delta=1. / 1000.,
+node = Node(world, node_name="TestWorld0", hidden=True, clock_delta=1. / 100.,
             world_masters_node_names=["Test1"])
 
 # Running node
