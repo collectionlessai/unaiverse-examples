c0f52a200c23ee38391db0dd82ffe421510d02ac
Stefano Melacci <stefano.melacci@unisi.it>
2026-05-09 14:06:51 +0200

    .

--- worlds/chat/run_w.py
+++ worlds/chat/run_w.py
@@ -3,7 +3,6 @@ from unaiverse.networking.node.node import Node
 
 # World
 world = WWorld()
-# world.debug_stats_dashboard()
 
 # Node hosting world
 node = Node(world, node_name="ChatRoom", hidden=True, clock_delta=1. / 20., world_masters_node_names=["Broadcaster"])
--- worlds/chat/src/world.py
+++ worlds/chat/src/world.py
@@ -71,7 +71,7 @@ class WWorld(World):
                           action="do_gen", args={"u_hashes": ["<agent>:processor_in"], "samples": 1},
                           ready=True, avoid_changing_ready=True)
         behav.add_transit("self_generated", "message_sent",
-                          action="ask_gen", args={"u_hashes": ["<agent>:processor"], "samples": 1, "ignore_uuid": True})
+                          action="ask_gen", args={"u_hashes": ["<agent>:processor"], "samples": 1})
 
         # Saving to file
         behav.save(os.path.join(self.world_folder, 'user.json'), only_if_changed=dummy_agent)
@@ -80,7 +80,7 @@ class WWorld(World):
         behav = HybridStateMachine(dummy_agent)
         behav.set_role("broadcaster")
 
-        behav.add_transit("ready", "ready", action="do_gen", args={"timeout": 5.0}, ready=False)
+        behav.add_transit("ready", "ready", action="do_gen", args={}, ready=False)
 
         # Saving to file
         behav.save(os.path.join(self.world_folder, 'broadcaster.json'), only_if_changed=dummy_agent)
