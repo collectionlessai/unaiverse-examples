a7b7a7dcc5ac5ffeb821abad437881ff192fd12b
Stefano Melacci <stefano.melacci@unisi.it>
2026-05-11 17:16:13 +0200

    company_brain added

---
+++ worlds/company_brain/run_6.py
@@ -0,0 +1,21 @@
+from unaiverse.agent import Agent
+from unaiverse.networking.node.node import Node
+from unaiverse.streams.dataprops import StreamType
+from unaiverse.streams import DataStream, TokensStream
+
+# Agent
+agent = Agent(proc=None,
+              proc_inputs=[StreamType(data_type="text", pubsub=False, private_only=True)],
+              proc_outputs=[StreamType(data_type="text", pubsub=False, private_only=True)])
+
+# Robot stream
+stream = DataStream.create(TokensStream("sensor.csv"),
+                                   "sensor", "sensor_group", public=False, pubsub=True)
+stream.props.delta = 10.0
+agent.add_stream(stream)
+
+# Node hosting agent
+node = Node(agent, node_name="Robot", hidden=True, clock_delta=1. / 20.)
+
+# Running node
+node.run(join_world="CompanyBrain", interact_mode=True, role_preference="team_member")
