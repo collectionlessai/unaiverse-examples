d03d495597446f90d781409db3cdbeb6d8a24372
Stefano Melacci <stefano.melacci@unisi.it>
2026-05-11 16:56:20 +0200

    company_brain added

--- worlds/company_brain/src/dispatcher.py
+++ worlds/company_brain/src/dispatcher.py
@@ -28,7 +28,7 @@ class WAgent(Agent):
                      samples: int = 100, time: float = -1., timeout: float = -1.,
                      _requester: str | list | None = None, _request_time: float = -1., _request_uuid: str | None = None,
                      _completed: bool = False) -> bool:
-        """Broadcast the result of the generation to all the agents in this world (excluding the sender)."""
+        """Dispatch the result of the generation to all the agents in this world (excluding the sender)."""
 
         if self.behaving_in_world():
 
@@ -39,17 +39,17 @@ class WAgent(Agent):
             _, _my_peer_id = self.get_peer_ids()
             self._sender_name = self.all_agents[_requester].get_static_profile()['node_name']
 
-            log.misc(f"Broadcaster received a request from {_requester}, agent named {self._sender_name}, "
+            log.misc(f"Dispatcher received a request from {_requester}, agent named {self._sender_name}, "
                      f"where the current agent list is:\n{list(self.world_agents.keys())}\n"
-                     f"and the broadcaster peer ID is {_my_peer_id}")
+                     f"and the dispatcher peer ID is {_my_peer_id}")
 
             _requester = list(set(self.world_agents.keys()) - {_requester, _my_peer_id})
 
-            log.misc(f"Broadcaster decided the list of peers to send the message from "
+            log.misc(f"Dispatcher decided the list of peers to send the message from "
                      f"agent {self._sender_name} is then {_requester}")
 
             if len(_requester) == 0:
-                log.error("Broadcaster is skipping the generation procedure, since no recipients would be there")
+                log.error("Dispatcher is skipping the generation procedure, since no recipients would be there")
                 return False
 
             generated_msg = await super().do_gen(u_hashes, extra_hashes, samples, time, timeout,
@@ -76,7 +76,7 @@ class WAgent(Agent):
     def proc_callback_inputs(self, inputs):
         inputs = super().proc_callback_inputs(inputs)
 
-        # Broadcaster will add usernames at the beginning of the message
+        # Dispatcher will add usernames at the beginning of the message
         if self.behaving_in_world():
             for i, _input in enumerate(inputs):
                 if isinstance(_input, str):
