77661353ada6967936020c06fdbc8b8908f3852e
Stefano Melacci <stefano.melacci@unisi.it>
2026-05-11 20:37:01 +0200

    company_brain added

--- worlds/company_brain/src/brain.py
+++ worlds/company_brain/src/brain.py
@@ -106,7 +106,7 @@ class WAgent(Agent):
     async def on_tick(self):
         agents_in_world = self.get_connection_pool_manager().world_agents_set
         for _agent in agents_in_world:
-            if _agent in self._contacted:
+            if _agent in self._contacted or _agent == self.get_peer_id():
                 continue
             if (not self.get_connection_pool_manager().is_connected(_agent) and
                     _agent not in self._node_agents_waiting and _agent not in self.world_agents):
