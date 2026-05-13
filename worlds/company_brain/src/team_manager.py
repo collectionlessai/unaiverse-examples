4fd0ef06132a2bba6c1f66ba52454b27405105e0
tommasoguidi <tommaso.guidi.1998@gmail.com>
2026-05-13 15:15:32 +0200

    demo

--- worlds/company_brain/src/team_manager.py
+++ worlds/company_brain/src/team_manager.py
@@ -12,16 +12,10 @@
                  Code Repositories:  https://github.com/collectionlessai/
                  Main Developers:    Stefano Melacci (Project Leader), Christian Di Maio, Tommaso Guidi
 """
-from .brain import WAgent as Agent
+from .demo import DemoAgent as Agent
 
 
 class WAgent(Agent):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-
-    def hook_on_received_msgs(self, msg_tuples: list[tuple[any, int, float]], history_len: int):
-        return
-
-    def hook_on_zero_received_msgs(self, max_silence_seconds: float):
-        return
