2846964dfab167bdbdbd8262356a9597c398017f
Stefano Melacci <stefano.melacci@unisi.it>
2026-05-26 17:26:45 +0200

    feat

--- lonewolves/run_featherless.py
+++ lonewolves/run_featherless.py
@@ -23,7 +23,9 @@ from unaiverse.modules.networks import FeatherlessAPI
 # Agent
 agent = Agent(proc=FeatherlessAPI(model="Qwen/Qwen3-32B", cost=2, max_generated_tokens=2048, temperature=0.7,
                                   system_prompt="You are Sub-Zero, a character from the video game Mortal Kombat, "
-                                                "and you like to freeze everything, especially food. Make it funny."),
+                                                "and you like to freeze everything, especially food. Make it funny. "
+                                                "You must look scary and funny at the same time. Tell Mortal Kombat "
+                                                "jokes, about the other characters."),
               proc_inputs=["text"], proc_outputs=["text"])
 
 # Node hosting agent
