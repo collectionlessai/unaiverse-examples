c62b1225f53d04b5bc7cdceef2c1c1cda55612d6
Stefano Melacci <stefano.melacci@unisi.it>
2026-05-27 12:00:56 +0200

    fixed run

--- lonewolves/run_featherless.py
+++ lonewolves/run_featherless.py
@@ -21,18 +21,18 @@ from unaiverse.modules.networks import FeatherlessAPI
 """
 
 # Agent
-agent = Agent(proc=FeatherlessAPI(model="Qwen/Qwen3-32B", cost=2, max_generated_tokens=512,
+agent = Agent(proc=FeatherlessAPI(model="Qwen/Qwen3-32B", cost=2, max_tokens=512,
                                   temperature=0.1, top_p=0.1, top_k=1, frequency_penalty=0.0,
                                   presence_penalty=0.0, repetition_penalty=1.1, min_p=0.05,
                                   sampler={"chat_template_kwargs": {"enable_thinking": False}},
                                   system_prompt="You are Sub-Zero, a character from the video game Mortal Kombat, "
                                                 "and you like to freeze everything, especially food. Make it funny. "
                                                 "You must look scary and funny at the same time. Tell Mortal Kombat "
-                                                "jokes about the other characters."),
+                                                "jokes about the other characters. Please reply with short answers."),
               proc_inputs=["text"], proc_outputs=["text"])
 
 # Node hosting agent
-node_agent = Node(agent, node_name="Featherless", hidden=True, clock_delta=1. / 25.)
+node_agent = Node(agent, node_name="Featherless2", hidden=True, clock_delta=1. / 25.)
 
 # Running node
 node_agent.run()
