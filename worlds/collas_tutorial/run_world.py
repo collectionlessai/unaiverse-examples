c3604b42d2a0e4a8615bf32f6780054f4c66fcc8
Stefano Melacci <stefano.melacci@unisi.it>
2026-09-09 12:13:22 +0200

    hidden

--- worlds/collas_tutorial/run_world.py
+++ worlds/collas_tutorial/run_world.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     world = WWorld()
 
     # Node hosting world
-    node = Node(node_name="CoLLAsTutorial", hosted=world, hidden=False, clock_delta=1./10.,
+    node = Node(node_name="CoLLAsTutorial", hosted=world, hidden=True, clock_delta=1./10.,
                 world_masters_node_names=["CoLLAsTeacher"])
 
     # Running node
